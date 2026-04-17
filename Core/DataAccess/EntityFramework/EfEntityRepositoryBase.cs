16e395cfe11415eac93149c868956ef98e1e5420
Heyedos <hidosss@hotmail.com>
2026-04-17 18:33:17 +0300

    Add project files.

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,43 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class , IEntity , new()
+        where TContext : DbContext, new()
+        // TContext -> DbContext olup class değildir. Özel bir framework , paket diyelim. , instance ı olması yeterli.
+    {
+        public void Add(TEntity Entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(Entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public List<TEntity> GetAll()
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().ToList();
+            }
+        }
+
+        public TEntity GetOne(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+    }
+}
