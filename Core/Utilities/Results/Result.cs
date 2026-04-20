2557cd9240747cbe915246e0c14d30e24b8ab14f
Heyedos <hidosss@hotmail.com>
2026-04-20 16:26:16 +0300

    added Result classes

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public bool Success { get; }
+
+        public string Message { get; }
+
+        public Result(bool success,string message) : this(success)
+        {
+            Message = message;
+        }
+        // this yapısı ile alttaki default ctor'un da çalışmasını sağlıyoruz.
+        // sadece success gönderilirse alttaki , hem succes hem message gönderilirse ustteki calısır
+        public Result(bool success)
+        {
+            Success = success;
+        }
+    }
+}
