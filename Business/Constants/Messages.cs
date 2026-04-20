aa53420a65fae81f21ff38965d6629c0259e04a1
Heyedos <hidosss@hotmail.com>
2026-04-20 16:09:23 +0300

    added constant messages

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Urun eklendi";
+        public static string ProductDeleted = "Urun silindi";
+        public static string ProductUpdated = "Urun guncellendi";
+        public static string ProductsListed = "Urunler listelendi";
+        public static string ProductError = "Hata meydana geldi";
+    }
+}
